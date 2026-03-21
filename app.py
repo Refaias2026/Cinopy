348d0905366fcaf16287f3ac820d059373c84cbf
Refaias2026 <refaiasoliveira788@gmail.com>
2026-03-21 17:43:16 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,51 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import psycopg2
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+def get_connection():
+    return psycopg2.connect(DATABASE_URL)
+
+def init_db():
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS reviews (
+            id SERIAL PRIMARY KEY,
+            texto TEXT
+        )
+    """)
+    conn.commit()
+    cur.close()
+    conn.close()
+
+init_db()
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+    if request.method == "POST":
+        texto = request.form.get("texto")
+
+        conn = get_connection()
+        cur = conn.cursor()
+        cur.execute("INSERT INTO reviews (texto) VALUES (%s)", (texto,))
+        conn.commit()
+        cur.close()
+        conn.close()
+
+        return redirect("/")
+
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT texto FROM reviews")
+    reviews = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    return render_template("index.html", reviews=reviews)
 
 if __name__ == "__main__":
     app.run()
