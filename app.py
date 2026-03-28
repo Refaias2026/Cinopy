9f6f1501a99ab1ecfeb483903df9a5e43f9beb5e
Refaias2026 <refaiasoliveira788@gmail.com>
2026-03-27 23:09:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -118,9 +118,4 @@ def sobre():
     return render_template("sobre.html")
 if __name__ == "__main__":
     app.run()
-    @app.after_request
-def add_header(response):
-    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
-    response.headers["Pragma"] = "no-cache"
-    response.headers["Expires"] = "0"
-    return response
+    
