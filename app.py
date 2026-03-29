1da56c5758263537698d0d9c5f2513befc6b0184
Refaias2026 <refaiasoliveira788@gmail.com>
2026-03-29 20:31:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,9 @@ init_db()
 # ===== REMOVER CACHE =====
 @app.after_request
 def add_header(response):
-    response.cache_control.no_store = True
+    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
     return response
 
 
